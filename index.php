f8819d230d12c03ab6369eb8b4fe67dd25a8db94
Maher Elshimy <maher.elshimy22@gmail.com>
2024-01-15 00:38:10 +0200

    Connect with DB

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 $con = mysqli_connect("localhost" ,"root","","shark management" );
 
 $phone = $_POST['phone'];
-$user_id = $_POST["user_id"] ; 
+$user_id = $_POST['user_id'] ; 
 
 mysqli_query($con, "INSERT INTO `phone` (`phone`, `user_id`) VALUES ('$phone','$user_id')");
 
+if (mysqli_affected_rows($con)==1) {
+    echo "Inserted" ; 
+} elseif (mysqli_affected_rows($con) == -1 ) {
+    echo "Syntax error" ;  
+} else { 
+    echo "Not inserted";
+}
 
 ?>
