63e1b3ddefefd9f0f24d26f70ed45c6226429eb9
Maher Elshimy <maher.elshimy22@gmail.com>
2024-01-15 01:09:35 +0200

    aaaa

--- index.php
+++ index.php
@@ -8,10 +8,15 @@ $con = mysqli_connect("localhost" ,"root","","shark management" );
 // mysqli_query($con, "INSERT INTO `phone` (`phone`, `user_id`) VALUES ('$phone','$user_id')");
 
 // $sql = "DELETE FROM `phone` WHERE `id` = 2 " ;
-$sql = "UPDATE `phone` SET `phone` = 3333 WHERE `id` = 3 " ;
+// $sql = "UPDATE `phone` SET `phone` = 3333 WHERE `id` = 3 " ;
+// $sql = "SELECT * FROM `phone` " ;
+$sql = "SELECT * FROM `student` " ;
+$my_queryy = mysqli_query ($con, $sql );
+$data = mysqli_fetch_all($my_queryy ,1);
 
-
-mysqli_query ($con, $sql );
+// echo "<pre>";
+// print_r($data);
+// echo "</pre>";
 
 if (mysqli_affected_rows($con)==1) {
     echo "Inserted" ; 
@@ -22,3 +27,20 @@ if (mysqli_affected_rows($con)==1) {
 }
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php foreach ($data as $row ) :?>
+        <li><?php echo $row['name'] ?> </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
