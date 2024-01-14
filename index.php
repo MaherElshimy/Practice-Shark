9ae1b1bf95f0aa19064c02b882c7d0d2dc9592d5
Maher Elshimy <maher.elshimy22@gmail.com>
2024-01-15 00:55:40 +0200

    second commit

--- index.php
+++ index.php
@@ -2,17 +2,23 @@
 
 $con = mysqli_connect("localhost" ,"root","","shark management" );
 
-$phone = $_POST['phone'];
-$user_id = $_POST['user_id'] ; 
+// $phone = $_POST['phone'];
+// $user_id = $_POST['user_id'] ; 
 
-mysqli_query($con, "INSERT INTO `phone` (`phone`, `user_id`) VALUES ('$phone','$user_id')");
+// mysqli_query($con, "INSERT INTO `phone` (`phone`, `user_id`) VALUES ('$phone','$user_id')");
+
+// $sql = "DELETE FROM `phone` WHERE `id` = 2 " ;
+$sql = "UPDATE `phone` SET `phone` = 3333 WHERE `id` = 3 " ;
+
+
+mysqli_query ($con, $sql );
 
 if (mysqli_affected_rows($con)==1) {
     echo "Inserted" ; 
 } elseif (mysqli_affected_rows($con) == -1 ) {
     echo "Syntax error" ;  
 } else { 
-    echo "Not inserted";
+    echo "Not inserted";    
 }
 
 ?>
