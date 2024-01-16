4782d04a75de268236fc8dab49e0104fb4a2e494
Maher Elshimy <maher.elshimy22@gmail.com>
2024-01-16 02:50:26 +0200

    OOP Calc

---
+++ OOP/index.php
@@ -0,0 +1,54 @@
+<?php
+
+class calc { 
+    public $number1 ; 
+    public $number2 ; 
+    public $result ; 
+
+
+    function add(){
+        $this->result = $this->number1 + $this->number2 ;
+        return $this->result; 
+    }
+
+    function sub(){
+        $this->result = $this->number1 - $this->number2 ;
+        return $this->result; 
+    }
+
+    function mult(){
+        $this->result = $this->number1 * $this->number2 ;
+        return $this->result; 
+    }
+
+    function div(){
+        $this->result = $this->number1 / $this->number2 ;
+        return $this->result; 
+    }
+
+    function print_calc(){
+        echo $this->result;
+    }
+}
+
+
+$object =new Calc(); 
+$object->number1 = 20 ;
+$object->number2 = 10 ;
+$object->add();
+$object->print_calc();
+echo "<hr>";
+$object->sub();
+$object->print_calc();
+echo "<hr>";
+$object->mult();
+$object->print_calc();
+echo "<hr>";
+$object->div();
+$object->print_calc();
+
+echo "<hr>";
+
+
+
+?>
